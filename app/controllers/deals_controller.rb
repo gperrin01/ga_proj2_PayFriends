4c7dce9c33df5a754164120b99243acfd29c1c0d
Guillaume Perrin <gperrin01@gmail.com>
2015-06-09 20:47:38 +0100

    back to ok to login after an unssuccefull trial at putting more html onto the layout. Becuase of login page I cannot put much on the layout

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,7 +1,7 @@
 class DealsController < ApplicationController
 
   def index
-    session[:logged_in] = User.find(params[:id])
+    session[:logged_in] = User.find(params[:logged_id])
     @pending = @deals.unsettled_deals.sort_by(&:updated_at)
     @history = @deals.settled_deals.sort_by(&:created_at)
   end
