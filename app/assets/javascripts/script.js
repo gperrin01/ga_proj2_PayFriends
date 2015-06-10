277b4ea8ae307d5267cebb4340a99338216ac5f2
Guillaume Perrin <gperrin01@gmail.com>
2015-06-10 09:42:30 +0100

    did ajax for add new friend and append to the dropdowns

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,17 +1,3 @@
-// function logIn(event) {
-//   event.preventDefault();
-//   console.log('login');
-//   var userName = $('#login').val();
-
-//   $.ajax({
-//     type: 'POST',
-//     url: '/users',
-//     dataType: 'json',
-//     data: {name: userName}  
-//   })
-// }
-
-
 function createUser(event) {
   event.preventDefault();
   console.log('create user');
@@ -23,15 +9,19 @@ function createUser(event) {
     dataType: 'json',
     data: {name: userName}  
   }).done(function(data){
-    // debugger;
-    // appendNewUser(data);
+    appendNewUser(data);
     console.log('success');
     console.log(data);
   })
 }
 
+function appendNewUser(data){
+  new_item = "<option value=" +data.id+ ">"+data.name+"</option>";
+  $('#deal_payer').append(new_item);
+  $('#deal_receiver').append(new_item);
+}
+
 
 $(document).ready(function() {
   $('#new_user').on('submit', createUser);
-  // $('#login').on('submit', logIn);
 })
